bdba48896eedaf744b304a56b79cf11f72a3672f
zerotouch-komal <komalzerotouch@gmail.com>
2025-11-04 14:52:58 +0530

    Updates

--- src/utils/configLoader.jsx
+++ src/utils/configLoader.jsx
@@ -84,7 +84,7 @@ export async function fetchConfigFromAPI() {
   
   try {
     console.log("Fetching fresh config from API...");
-    const apiUrl = `http://192.168.1.106:3001/api/companies/config`;
+    const apiUrl = `https://app.101ewarranty.com/api/companies/config`;
     console.log("API URL:", apiUrl);
     
     const res = await fetch(apiUrl, {
